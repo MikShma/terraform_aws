433a347b622aa59d6b31d81d6618924778430f9f
M S <ms@example.com>
2021-10-14 17:53:54 +0300

    add ELB

--- computeEC2/launchtpl_autoscale.tf
+++ computeEC2/launchtpl_autoscale.tf
@@ -1,4 +1,4 @@
-
+## Creating Launch Configuration
 resource "aws_launch_template" "web_app" {
   name = "web_app"
 
@@ -25,9 +25,10 @@ resource "aws_launch_template" "web_app" {
     }
   }
 
-  user_data = filebase64(templatefile("${path.module}/userdata.tpl", {firewall_subnets = var.subnet_ips[0]}))
+  user_data = base64encode(templatefile("${path.module}/userdata.tpl", {firewall_subnets = var.subnet_ips[0]}))
 }
 
+## Creating AutoScaling Group
 resource "aws_autoscaling_policy" "example" {
   name = "cpu_autoscale_policy"
   policy_type = "TargetTrackingScaling"
@@ -50,5 +51,65 @@ resource "aws_autoscaling_group" "web_app" {
   launch_template {
     id      = aws_launch_template.web_app.id
     version = "$Latest"
+  }
+
+  health_check_type = "ELB"
+  target_group_arns = [aws_lb_target_group.tf_target_gr.arn]
+}
+
+### Creating ELB
+resource "aws_lb" "tf_elb" {
+  name               = "tf_web_app_lb"
+  load_balancer_type = "application"
+
+  subnets  = var.subnets
+  security_groups = [var.security_group]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.tf_elb.arn 
+  port = 80
+  protocol = "HTTP"
+
+  // By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "tf_target_gr" {
+  name = "tf_target_gr"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.tf_target_gr.arn
+  }
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
   }
 }
